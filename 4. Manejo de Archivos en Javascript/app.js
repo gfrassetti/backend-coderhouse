c1a66d1ccc072335de4a20b6889f697b40646979
Guido Frassetti <78879523+gfrassetti@users.noreply.github.com>
2022-06-06 21:35:12 -0300

    update

--- 4. Manejo de Archivos en Javascript/app.js
+++ 4. Manejo de Archivos en Javascript/app.js
@@ -24,8 +24,77 @@ class Container {
 
       }
   }
+
+  async getById(id){
+    let products = await JSON.parse(fs.prosmises.readFile('./products.txt', 'utf-8'))
+    
+    try{
+      let object = products.find((prod) => prod.id  == id)
+    }
+    catch{
+      console.log(`Product does not exists with id ${id}`)
+      
+    }
+  }
+  async getAll() {
+    try {
+      let productos = JSON.parse(
+        await fs.promises.readFile("./products.txt", "utf-8")
+      );
+      console.log(productos);
+    } catch (error) {throw error}
+  }
+  async deleteById(id) {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./productos.txt", "utf-8")
+      );
+      
+      try {
+      
+        if (productos.some((prod) => prod.id == id)) {
+        let newProductos = productos.filter((prod) => prod.id != id);
+
+        await fs.promises.writeFile(
+          `./${this.archivo}`,
+          JSON.stringify(newProductos)
+        );
+        console.log("producto eliminado");
+      } else {
+        console.log("no existe producto con ese id");
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteAll() {
+    
+    let archivo = await fs.promises.readFile(`./${this.archivo}`, "utf-8");
+    
+    try {
+    
+      if (!archivo) {
+        console.log("Archivo no existe");
+      } else {
+        await fs.promises.writeFile(`./${this.archivo}`, "[]");
+
+        console.log("Todos los archivos han sido eliminados");
+      }
+    } catch (error){
+      throw error;
+    }
+  }
 }
+
 
 const producto = new Container('products.txt')
 
 producto.save({name: 'notebook'}).then(id => console.log(id))
+
+producto.getById(1)
+
+//producto.getAll()
+
+//producto.deleteById(2)
+//producto.deleteAll(2)
+
